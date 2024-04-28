4844bbc14802d894fb4b95fc484e97fd3fe5c1c5
Rafael-Benicio <jerobenicio@gmail.com>
2024-04-28 16:20:08 -0300

    Add: Adiciona position atribute

--- src/base/player.rs
+++ src/base/player.rs
@@ -9,6 +9,7 @@ use sdl2::{
 };
 
 pub struct Player {
+    position:Vector2D<i32>,
     rect: Rect,
     color: Color,
     acceleration: u8,
@@ -22,6 +23,7 @@ pub struct Player {
 impl Player {
     pub fn new(size: (u32, u32)) -> Self {
         Player {
+            position:Vector2D::new((size.0 * 6) as i32, (size.1 * 14) as i32),
             rect: Rect::new((size.0 * 6) as i32, (size.1 * 14) as i32, size.0, size.1),
             fisic_body: CollisionBody::new(
                 (size.0 * 6) as i32,
@@ -29,9 +31,9 @@ impl Player {
                 size.0,
                 size.1,
             ),
-            direction: Vector2D { x: 0, y: 0 },
+            direction: Vector2D::new(0,0),
             max_vel: 10,
-            momentum: Vector2D { x: 0, y: 0 },
+            momentum: Vector2D::new(0,0),
             acceleration: 2,
             momentum_frame_counter: 0,
             color: Color::RGB(255, 255, 255),
@@ -69,14 +71,18 @@ impl Control for Player {
     }
 
     fn set_position(&mut self, x: i32, _y: i32) {
-        self.rect.x = if x < 0 {
+
+        self.position.x = if x < 0 {
+            self.momentum.x-=self.momentum.x;
             0
         } else if x + self.fisic_body.proportions.x as i32 > WINDOW_WIDTH as i32 {
+            self.momentum.x-=self.momentum.x;
             WINDOW_WIDTH as i32 - self.fisic_body.proportions.x as i32
         } else {
             x
         };
-        self.fisic_body.set_position(self.rect.x, 0);
+        self.rect.x=self.position.x;
+        self.fisic_body.set_position(self.position.x, 0);
     }
 
     fn update(&mut self) {
@@ -104,7 +110,7 @@ impl Control for Player {
             self.momentum_frame_counter += 1;
         }
 
-        self.set_position(self.rect.x + self.momentum.x, 0);
+        self.set_position(self.position.x + self.momentum.x, 0);
     }
 }
 
--- src/base/vector2d.rs
+++ src/base/vector2d.rs
@@ -3,3 +3,9 @@ pub struct Vector2D<T> {
     pub x: T,
     pub y: T,
 }
+
+impl<T> Vector2D<T>{
+    pub fn new(x:T,y:T)->Vector2D<T>{
+        Vector2D{x:x,y:y}
+    }
+}
